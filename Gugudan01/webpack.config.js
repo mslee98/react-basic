2a8fbe5abe2dad8b7e4b681f15848238b46298c3
mslee98 <lms980321@kakao.com>
2023-08-18 09:23:06 +0900

    'webpack-setting'

--- Gugudan01/webpack.config.js
+++ Gugudan01/webpack.config.js
@@ -18,10 +18,22 @@ module.exports = {
             test: /\.jsx?$/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
+                // presets도 상세 설정을 줄수 있음
+                // presets: ['@babel/preset-env', '@babel/preset-react']
+                presets: [
+                    ['@babel/preset-env', {
+                        target: {
+                            browsers: ['last 2 chrome versions'] //이런식으로 적게되면 현재 크롬브라우저가 70이라고 하면 69, 70버전에 맞춰 호환할 수 있게 지원한다. browser 리스트 보면 다양한 사용방법들이 있으니 참고
+                        },
+                    }],
+                ],
+                plugins: [],
             },
         }],
     },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({debug: true}),
+    ],
 
     output: {
         path: path.join(__dirname + '/dist'),
