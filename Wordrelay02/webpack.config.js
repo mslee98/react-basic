2388b06e4ad3e99faf1686bd0854f42bab7e458f
mslee98 <lms980321@kakao.com>
2023-08-18 17:46:01 +0900

    '끝말있기 웹팩 설정'

--- Wordrelay02/webpack.config.js
+++ Wordrelay02/webpack.config.js
@@ -27,13 +27,26 @@ module.exports = {
 
    module: {
         rules: [{
-            test: /\.jsx?$/,
+            test: /\.jsx?$/, // 정규 표현식 보면 그래도 공부는 해봐야할 것 같음
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
+                // presets도 상세 설정을 줄수 있음
+                // presets: ['@babel/preset-env', '@babel/preset-react']
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            //이런식으로 적게되면 현재 크롬브라우저가 70이라고 하면 69, 70버전에 맞춰 호환할 수 있게 지원한다. browser 리스트 보면 다양한 사용방법들이 있으니 참고
+                            // 아래 보면 한국에서 점유율이 5% 이상인 브라우저들과 크롬 최신 버전 2개를 뜻함
+                            browsers: ['> 5% in KR', 'last 2 chrome versions'] 
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
+                ],
+                // plugins: ['@babel/plugin-syntax-jsx'],
             },
         }],
-   },
+    },
 
    // *출력*
    // node에 path.join을 사용하여 현재폴더 + dist 내로 입력한 파일들을 app.js로 출력한다.
