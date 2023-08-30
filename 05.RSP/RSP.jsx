358fbbce6649d094875aa97b888dba352f57eeaa
mslee98 <lms980321@kakao.com>
2023-08-30 14:42:46 +0900

    'RSP init setting'

---
+++ 05.RSP/RSP.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class RSP extends Component {
+    render() {
+        return (
+            <>
+            dddddddddd
+            </>
+        )
+    }
+}
+
+export default RSP;
