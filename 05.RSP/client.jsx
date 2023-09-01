896b72c7b84b67b2e80f3a38ec03c88a23a648d0
mslee <lms980321@kakao.com>
2023-09-01 15:36:30 +0900

    'functional finished'

--- 05.RSP/client.jsx
+++ 05.RSP/client.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import RSP from './RSP-class';
+// import RSP from './RSP-class';
+import RSP from './RSP-functional';
 
 ReactDOM.render( <RSP />, document.querySelector("#root"));
